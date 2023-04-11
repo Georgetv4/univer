d3beef21b41286befe01d8e682a574c331cdff85
George Tverdokhlib <george.tverdokhlib@gmail.com>
2023-04-11 18:51:41 +0300

    complete task 3

--- Homework2/task3.py
+++ Homework2/task3.py
@@ -1,4 +1,10 @@
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-def check(email):
-    for i in email:
+import re
 
+def check_email(email):
+    pattern = r'^[a-zA-Z][\w]*@[a-zA-Z]+\.[a-zA-Z]{2,3}$'
+    if re.match(pattern, email):
+        return "The email you entered is valid."
+    else:
+        return "The email you entered is not valid."
+
+print(check_email('george_tverdokhlib@gmail.com'))
