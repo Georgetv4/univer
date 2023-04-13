6b2b68d0f0e62e3a133792eafaac7eecba267fd2
George Tverdokhlib <george.tverdokhlib@gmail.com>
2023-04-13 14:26:02 +0300

    update task 6

--- Homework2/task6.py
+++ Homework2/task6.py
@@ -1,14 +1,9 @@
-# alphabet = 'abcdefghijklmnopqrstuvwxyz'
-# def find_missing_letter(chars):
-#     for i in range(len(chars) - 1):
-#         if ord(chars[i+1]) - ord(chars[i]) > 1:
-#             return chr(ord(chars[i]) + 1)
-
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
 def find_missing_letter(chars):
     for i in range(len(chars) - 1):
-        if chars.find(chars[i]) - chars.find(chars[i-1]) > 1:
-            return alphabet[chars.find(chars[i])]
+        if ord(chars[i+1]) - ord(chars[i]) > 1:
+            return chr(ord(chars[i]) + 1)
+    else:
+        return "There is no missing letters"
 
 
-print(find_missing_letter('abcdfg'))
+print(find_missing_letter('abcdefg'))
