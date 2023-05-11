00dd7940a06b49025ae3f2034acb3b1dd0e31647
George Tverdokhlib <george.tverdokhlib@gmail.com>
2023-05-11 13:34:11 +0300

    update task 6

--- Homework2/task6.py
+++ Homework2/task6.py
@@ -1,9 +1,9 @@
 def find_missing_letter(chars):
     for i in range(len(chars) - 1):
         if ord(chars[i+1]) - ord(chars[i]) > 1:
-            return chr(ord(chars[i]) + 1)
+            return print('The missing letter is:', chr(ord(chars[i]) + 1))
     else:
         return "There is no missing letters"
 
 
-print(find_missing_letter('abcdefg'))
+find_missing_letter('abcefg')
